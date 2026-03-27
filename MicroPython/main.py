015c17e378df53fe305b605c752f13a3e44a5def
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-03-27 17:20:43 +0000

    changed main.py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,52 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Kyle Matthew
+Created on: Mar 2026
+This module reads light levels and updates NeoPixels.
 """
 
 from microbit import *
+import neopixel
 
-display.scroll("Hello, World!")
+# Initialize NeoPixels
+strip = neopixel.NeoPixel(pin16, 4)
+
+# setup
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        sleep(100)
+
+        # Read the light level (0-255)
+        light_reading = display.read_light_level()
+        display.scroll(str(light_reading), wait=False)
+
+        # 1. Start with all pixels off
+        strip.clear()
+
+        # 2. If light is > 52, light up 1st Neopixel (Red)
+        if light_reading > 52:
+            strip[0] = (255, 0, 0)
+
+        # 3. If light is > 104, light up 2nd Neopixel
+        if light_reading > 104:
+            strip[1] = (255, 0, 0)
+
+        # 4. If light is > 156, light up 3rd Neopixel
+        if light_reading > 156:
+            strip[2] = (255, 0, 0)
+
+        # 5. If light is > 208, light up 4th Neopixel
+        if light_reading > 208:
+            strip[3] = (255, 0, 0)
+
+        # Show the final result
+        strip.show()
+
+        # Wait 2 seconds then reset
+        sleep(2000)
+        strip.clear()
+        strip.show()
+        display.show(Image.HAPPY)
