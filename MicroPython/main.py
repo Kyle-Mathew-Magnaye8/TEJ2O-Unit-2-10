4c888694129168ab7d35fc226678c26764222b69
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-04-02 17:23:10 +0000

    changed code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,7 +7,7 @@ This module reads the light levels and updates NeoPixels.
 from microbit import *
 import neopixel
 
-# Initialize NeoPixels
+# variables
 strip = neopixel.NeoPixel(pin16, 4)
 
 # setup
@@ -19,33 +19,33 @@ while True:
         display.clear()
         sleep(100)
 
-        # Read the light level (0-255)
+        # read the light level (0-255)
         light_reading = display.read_light_level()
         display.scroll(str(light_reading), wait=False)
 
-        # 1. Start with all pixels off
+        # 1. start with all pixels off
         strip.clear()
 
-        # 2. If light is > 52, light up 1st Neopixel (Red)
+        # 2. if light is > 52, light up 1st Neopixel (Red)
         if light_reading > 52:
             strip[0] = (255, 0, 0)
 
-        # 3. If light is > 104, light up 2nd Neopixel
+        # 3. if light is > 104, light up 2nd Neopixel
         if light_reading > 104:
             strip[1] = (255, 0, 0)
 
-        # 4. If light is > 156, light up 3rd Neopixel
+        # 4. if light is > 156, light up 3rd Neopixel
         if light_reading > 156:
             strip[2] = (255, 0, 0)
 
-        # 5. If light is > 208, light up 4th Neopixel
+        # 5. if light is > 208, light up 4th Neopixel
         if light_reading > 208:
             strip[3] = (255, 0, 0)
 
         # Show the final result
         strip.show()
 
-        # Wait 2 seconds then reset
+        # wait 2 seconds then reset
         sleep(2000)
         strip.clear()
         strip.show()
